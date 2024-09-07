85267cc6f81c30fe552e4c7d7bea0cc1b5c123ab
Raihan Rafeek <raihanrafeek@gmail.com>
2024-09-07 19:14:16 -0400

    patch: Adding post-hackathon fixes.

--- scripts/DB_labels_fix.py
+++ scripts/DB_labels_fix.py
@@ -3,9 +3,9 @@ from neo4j import GraphDatabase
 
 LABELS_QUERY="CALL db.labels() YIELD label RETURN label"
 
-uri = "neo4j+s://f9abbebf.databases.neo4j.io"  
+uri = "bolt://localhost:7687"  
 username = "neo4j"         
-password = "sF2U5HO-3zyrCrwRRYKkTu9G-TTHZlh4kRL5_HFbRqQ"
+password = "Security.4u"  
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
 def checkUnderscore(word):
@@ -18,25 +18,28 @@ with driver.session() as session:
 
 similarity_threshold = 89
 
-similar_labels = []
-for i in range(len(labels)):
-    for j in range(i + 1, len(labels)):
-        label1 = checkUnderscore(labels[i])
-        label2 = checkUnderscore(labels[j])
-         
-        similarity = fuzz.ratio(label1, label2)
-        if similarity >= similarity_threshold:
-            similar_labels.append((label1, label2, similarity))
+for i in range(4):
+    similar_labels = []
+    for i in range(len(labels)):
+        for j in range(i + 1, len(labels)):
+            label1 = checkUnderscore(labels[i])
+            label2 = checkUnderscore(labels[j])
+            
+            similarity = fuzz.ratio(label1, label2)
+            if similarity >= similarity_threshold:
+                similar_labels.append((label1, label2, similarity))
 
-for label1, label2, similarity in similar_labels:
-    with driver.session() as session:
-        query = f"""
-        MATCH (n) 
-        WHERE (n:`{label1}`) OR (n:`{label2}`) 
-        SET n:`{label1}` 
-        REMOVE n:`{label2}`
-        """
-        session.run(query)
-        print(f"Merged '{label2}' into '{label1}' and removed '{label2}'")
+    for label1, label2, similarity in similar_labels:
+        with driver.session() as session:
+            query = f"""
+            MATCH (n) 
+            WHERE (n:`{label1}`) OR (n:`{label2}`) 
+            SET n:`{label1}` 
+            REMOVE n:`{label2}`
+            """
+            session.run(query)
+            print(f"Merged '{label2}' into '{label1}' and removed '{label2}'")
+    
+    print(F"\n\n *****ROUND {i} DONE*****")
     
 print("Done!")
